5f0e9e42f365cfd021f4d5b28b75d4647ffde79c
Kamil Kolanowski <tanssive@yahoo.com>
2023-11-17 00:38:45 +0100

    changed main method

--- Hangman.py
+++ Hangman.py
@@ -5,8 +5,8 @@ import random
 class Hangman:
     WRONG_GUESSES = 0
 
-    def __init__(self):
-        pass
+    def __init__(self, user_name):
+        self.user_name = user_name
 
 
     def replace_word_with_blank_lines(self, word):
@@ -25,25 +25,36 @@ class Hangman:
         for idx, _ in enumerate(word_lst):
             if letter in word_lst[idx]:
                 blanks_lst[idx] = letter
-            else:
-                self.WRONG_GUESSES += 1
 
         return ''.join(list(blanks_lst))
     
 
     def game(self):
         chances = 0
+
         generated_word = self.generate_word()
         blanked_word = self.replace_word_with_blank_lines(generated_word)
-        print(f"Generated word: {generated_word}")
+        blank_word_to_display = ' '. join(list(blanked_word))
 
-        while chances <= len(generated_word) :
+        print(f"Your word has {len(blanked_word)} characters: {blank_word_to_display}")
+
+        while chances <= 7:
             user_guess = self.get_user_guess()
             blanked_word = self.check_letter_guess_with_word(user_guess, generated_word, blanked_word)
-            print(blanked_word)
-            if user_guess not in blanked_word:
-                print(f"It's not the '{user_guess}'! Try again! \n {self.draw_hanged_man(chances)}")
+
+            if user_guess not in blanked_word and self.WRONG_GUESSES < 7:
+                print(f'It\'s not the "{user_guess}"! Try again! \n {self.draw_hanged_man(self.WRONG_GUESSES)}')
                 chances += 1
+                self.WRONG_GUESSES += 1
+            elif self.WRONG_GUESSES <= 7:
+                print(f"{self.user_name} you lost! The word was: {generated_word}")
+                chances = 7
+            elif blanked_word == generated_word:
+                print(f'Congratulations {self.user_name} you guessed the word "{generated_word}" and you won!')
+                chances = 7
+            else:
+                print(f"Word: {blanked_word}")
+            
 
     @staticmethod
     def generate_word():
@@ -161,9 +172,5 @@ class Hangman:
 
 
 if __name__ == '__main__':
-    chances = 0
-    hangman = Hangman()
-
+    hangman = Hangman('Kamil')
     hangman.game()
-    # while chances < 5:
-    #     print(hangman.check_letter_guess_with_word())
