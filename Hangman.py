6ee95a2d0c2c1161bd7fbbd8e3d68e34e95ba402
Kamil Kolanowski <tanssive@yahoo.com>
2023-11-22 16:44:08 +0100

    remove redundant print & change loop checking word

--- Hangman.py
+++ Hangman.py
@@ -35,14 +35,14 @@ class Hangman:
         generated_word = self.generate_word()
         blanked_word = self.replace_word_with_blank_lines(generated_word)
         blank_word_to_display = ' '. join(list(blanked_word))
-        print(generated_word)
+ 
         print(f"Your word has {len(blanked_word)} characters: {blank_word_to_display}")
 
         while chances <= 6:
             user_guess = self.get_user_guess()
             blanked_word = self.check_letter_guess_with_word(user_guess, generated_word, blanked_word)
             
-            if user_guess not in blanked_word and self.WRONG_GUESSES <= 6:
+            if user_guess not in blanked_word and self.WRONG_GUESSES < 7:
                 print(f'It\'s not the "{user_guess}"! Try again!\n{self.draw_hanged_man(self.WRONG_GUESSES)}\n{blanked_word}')
                 chances += 1
                 self.WRONG_GUESSES += 1
